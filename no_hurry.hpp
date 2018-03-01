b163ca531b4930ea3c8b9147d82ebecdef727f2a
Darius <marian.darius98@gmail.com>
2018-03-01 13:05:53 -0800

    * no hurry x3

--- no_hurry.hpp
+++ no_hurry.hpp
@@ -12,6 +12,10 @@ using namespace std;
 
 void PopulateRides(vector<Ride> &all_rides, auto &output) {
     set<Ride> available_rides(all_rides.begin(), all_rides.end());
+    map<int, Ride> ride_ids;
+    for (auto ride: all_rides) {
+        ride_ids[ride.id] = ride;
+    }
     vector<int> available_vehicle_ids;
     map<int, pair<Point, int>> vehicle_location;
     map<int, vector<Ride>> vehicle_id_assigned_rides;
@@ -21,57 +25,76 @@ void PopulateRides(vector<Ride> &all_rides, auto &output) {
     }
 
     int totalPoints = 0;
-
-    while (!available_rides.empty() && !available_vehicle_ids.empty()) {
-        bool ok = false;
-
-        sort(available_vehicle_ids.begin(), available_vehicle_ids.end(), [&](const int v1, const int v2) {
-            return vehicle_location[v1].second < vehicle_location[v2].second;
-        });
-
-        int vehicle_id;
-        if (available_vehicle_ids.size() > 1) {
-            vehicle_id = available_vehicle_ids[RandLog(1, (int)available_vehicle_ids.size()) - 1];
-        } else {
-            vehicle_id = available_vehicle_ids[0];
-        }
-
-        vector<Ride> ok_rides;
-
-        for (Ride ride : available_rides) {
-            pair<Point, int> current_location = vehicle_location[vehicle_id];
-            int startTime = current_location.second + current_location.first.Distance(ride.start);
-            int points = ride.Points(startTime);
-            if (points > 0) {
-                ok_rides.push_back(ride);
-                ok = true;
-            }
-        }
-
-        if (ok) {
-            sort(ok_rides.begin(), ok_rides.end(), [&](const Ride &r1, const Ride &r2) {
-                pair<Point, int> current_location = vehicle_location[vehicle_id];
-                return current_location.first.Distance(r1.start) < current_location.first.Distance(r2.start);
-            });
-
-            Ride selected_ride;
-            if (ok_rides.size() >= 2) {
-                selected_ride = ok_rides[RandLog(1, (int)ok_rides.size()) - 1];
-            } else {
-                selected_ride = ok_rides[0];
-            }
+    ifstream cin("answers/test-2.ans");
+    for (int i = 0; i < num_vehicles; ++ i) {
+        int num_routes;
+        cin >> num_routes;
+        for (int ride_id, j = 0; j < num_routes; ++ j) {
+            cin >> ride_id;
+            int vehicle_id = i;
+            Ride selected_ride = ride_ids[ride_id];
             available_rides.erase(selected_ride);
             vehicle_id_assigned_rides[vehicle_id].push_back(selected_ride);
             pair<Point, int> current_location = vehicle_location[vehicle_id];
             int startTime = current_location.second + current_location.first.Distance(selected_ride.start);
             totalPoints += selected_ride.Points(startTime);
             vehicle_location[vehicle_id] = {selected_ride.destination, selected_ride.FinishTime(startTime)};
-        } else {
-            available_vehicle_ids.erase(find(available_vehicle_ids.begin(), available_vehicle_ids.end(), vehicle_id));
+
         }
     }
+    cin.close();
+
+//    while (!available_rides.empty() && !available_vehicle_ids.empty()) {
+//        bool ok = false;
+//
+//        sort(available_vehicle_ids.begin(), available_vehicle_ids.end(), [&](const int v1, const int v2) {
+//            return vehicle_location[v1].second < vehicle_location[v2].second;
+//        });
+//
+//        int vehicle_id;
+//        if (available_vehicle_ids.size() > 1) {
+//            vehicle_id = available_vehicle_ids[RandLog(1, (int)available_vehicle_ids.size()) - 1];
+//        } else {
+//            vehicle_id = available_vehicle_ids[0];
+//        }
+//
+//        vector<Ride> ok_rides;
+//
+//        for (Ride ride : available_rides) {
+//            pair<Point, int> current_location = vehicle_location[vehicle_id];
+//            int startTime = current_location.second + current_location.first.Distance(ride.start);
+//            int points = ride.Points(startTime);
+//            if (points > 0) {
+//                ok_rides.push_back(ride);
+//                ok = true;
+//            }
+//        }
+//
+//        if (ok) {
+//            sort(ok_rides.begin(), ok_rides.end(), [&](const Ride &r1, const Ride &r2) {
+//                pair<Point, int> current_location = vehicle_location[vehicle_id];
+//                return current_location.first.Distance(r1.start) < current_location.first.Distance(r2.start);
+//            });
+//
+//            Ride selected_ride;
+//            if (ok_rides.size() >= 2) {
+//                selected_ride = ok_rides[RandLog(1, (int)ok_rides.size()) - 1];
+//            } else {
+//                selected_ride = ok_rides[0];
+//            }
+//            available_rides.erase(selected_ride);
+//            vehicle_id_assigned_rides[vehicle_id].push_back(selected_ride);
+//            pair<Point, int> current_location = vehicle_location[vehicle_id];
+//            int startTime = current_location.second + current_location.first.Distance(selected_ride.start);
+//            totalPoints += selected_ride.Points(startTime);
+//            vehicle_location[vehicle_id] = {selected_ride.destination, selected_ride.FinishTime(startTime)};
+//        } else {
+//            available_vehicle_ids.erase(find(available_vehicle_ids.begin(), available_vehicle_ids.end(), vehicle_id));
+//        }
+//    }
     int bestPoints = totalPoints;
     bool tryUpgrade = false;
+    int index = 0;
     while (true) {
         bool upgraded = false;
         if (!tryUpgrade) {
@@ -79,7 +102,7 @@ void PopulateRides(vector<Ride> &all_rides, auto &output) {
             upgraded = true;
         } else {
             // Trying to upgrade a car
-            int vehicle_id = Rand(0, num_vehicles);
+            int vehicle_id = (index++) % num_vehicles;
 
             // save state
             vector<Ride> old_rides = vehicle_id_assigned_rides[vehicle_id];
@@ -129,13 +152,13 @@ void PopulateRides(vector<Ride> &all_rides, auto &output) {
 
             if (newPoints > oldPoints) {
                 bestPoints += newPoints - oldPoints;
-                cerr << "Success " << bestPoints << "\n";
+                cerr << "\rSuccess " << bestPoints;
                 upgraded = true;
             } else {
                 vehicle_id_assigned_rides[vehicle_id] = old_rides;
                 vehicle_location[vehicle_id] = old_location;
                 available_rides = old_available;
-                cerr << "Fail\n";
+                cerr << "\rFail";
             }
         }
         if (upgraded) {
